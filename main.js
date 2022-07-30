e796ffe5e86ef361b70246ea6dce2c23020c6936
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-07-29 22:03:59 -0400

    added function to load imgs

--- main.js
+++ main.js
@@ -3,4 +3,24 @@ let canvas = document.createElement("canvas");
 let ctx = canvas.getContext("2d");
 canvas.width = 400;
 canvas.height = 700;
-document.body.appendChild(canvas)
+document.body.appendChild(canvas);
+
+//Load Images
+let backGroundImg, bulletImg, GameOverImg, monsterImg, spaceshipImg;
+
+function loadImgs() {
+  backGroundImg = new Image();
+  backGroundImg.src = "images/SpaceImg.png";
+
+  bulletImg = new Image();
+  bulletImg.src = "images/Bullet.png";
+
+  GameOverImg = new Image();
+  GameOverImg.src = "images/GameOver.jpeg";
+
+  monsterImg = new Image();
+  monsterImg.src = "images/Monster.png";
+
+  spaceshipImg = new Image();
+  spaceshipImg.src = "images/SpaceShip.png";
+}
